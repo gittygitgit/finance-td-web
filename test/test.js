2d7364445d7953afa9352e3c168a405636d924ea
Michael Grudkowski <mgrudkowski@gmail.com>
2017-10-17 15:34:44 -0400

    initial commit

---
+++ test/test.js
@@ -0,0 +1,6 @@
+var path = require('path');
+var foo = require('../index.js');
+
+//console.log(foo.getSymbolArray());
+//console.log(foo.getSymbolArrayStartsWith('A'));
+console.log(foo.getSymbolJSONStartsWith('A'));
